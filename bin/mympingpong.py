b79763b987f1721580491597e14cef954d12f919
jerodcle <jeroen.declerck@ugent.be>
2015-07-06 09:36:16 +0200

    cleaned up comments in mympingpong

--- bin/mympingpong.py
+++ bin/mympingpong.py
@@ -33,7 +33,6 @@ TODO:
  - factor out the pingpong class in vsc.mympingpong.pingpong
  - refactor mypingpong class in regular main() function
 """
-
 # this needs to be imported before other loggers or fancylogger won't work
 from vsc.utils.generaloption import simple_option
 
